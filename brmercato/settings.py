43e7eae644a5cfa351fe6c49a92f3fb9bd24215c
asmaur <maur.a@outlook.com>
2018-04-26 09:50:29 -0300

    final step

--- brmercato/settings.py
+++ brmercato/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'brmercato.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db-mercato.sqlite3'),
     }
 }
 
